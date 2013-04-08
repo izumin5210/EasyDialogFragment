0a7c74ccec2402ef70f7ba91c9e4fc51611a13f2
Masayuki IZUMI <masayuki@izumin.info>
2013-04-08 23:46:19 +0900

    add .gitignore, README.md

--- gen/info/izumin/android/easydialogfragment/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package info.izumin.android.easydialogfragment;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
